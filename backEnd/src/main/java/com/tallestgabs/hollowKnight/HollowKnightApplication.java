d38f8b3f9d8d029621c8e56ffd97c4e84e1337e4
tallestgabs <gabrielcastrodias1414@gmail.com>
2024-11-22 10:45:09 -0300

    started front

---
+++ backEnd/src/main/java/com/tallestgabs/hollowKnight/HollowKnightApplication.java
@@ -0,0 +1,13 @@
+package com.tallestgabs.hollowKnight;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HollowKnightApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HollowKnightApplication.class, args);
+	}
+
+}
---
+++ backEnd/src/test/java/com/tallestgabs/hollowKnight/HollowKnightApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tallestgabs.hollowKnight;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HollowKnightApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
